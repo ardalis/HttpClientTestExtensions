c6e729a01cac12bdecaa3a168b89079f98547ab9
Steve Smith <steve@kentsmiths.com>
2021-02-09 17:02:33 -0500

    Initial version

---
+++ src/Ardalis.HttpClientTestExtensions/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Ardalis.HttpClientTestExtensions
+{
+    public static class Constants
+    {
+        public static JsonSerializerOptions DefaultJsonOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+    }
+}
---
+++ src/Ardalis.HttpClientTestExtensions/HttpClientExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Ardalis.HttpClientTestExtensions
+{
+    public static class HttpClientExtensionMethods
+    {
+        public static async Task<T> GetAndDeserialize<T>(this HttpClient client, string requestUri, ITestOutputHelper output = null)
+        {
+            var response = await client.GetAsync(requestUri);
+            output?.WriteLine($"Requesting {requestUri}");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            output?.WriteLine($"Response: {stringResponse}");
+            var result = JsonSerializer.Deserialize<T>(stringResponse,
+              Constants.DefaultJsonOptions);
+
+            return result;
+        }
+    }
+}
