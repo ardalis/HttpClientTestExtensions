7006eb8de58a716e086cae1027e9d8ff56fec5b5
ShadyNagy <info@shadynagy.com>
2022-07-26 01:39:33 +0200

    code style format.

--- tests/Ardalis.HttpClientTestExtensions.Tests/CustomWebApplicationFactory.cs
+++ tests/Ardalis.HttpClientTestExtensions.Tests/CustomWebApplicationFactory.cs
@@ -9,61 +9,59 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Ardalis.HttpClientTestExtensions.Tests
+namespace Ardalis.HttpClientTestExtensions.Tests;
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-  public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+  protected override IHost CreateHost(IHostBuilder builder)
   {
-    protected override IHost CreateHost(IHostBuilder builder)
+    var host = builder.Build();
+    host.Start();
+
+    var serviceProvider = host.Services;
+
+    using var scope = serviceProvider.CreateScope();
+    var scopedServices = scope.ServiceProvider;
+    var db = scopedServices.GetRequiredService<AppDbContext>();
+
+    var logger = scopedServices
+      .GetRequiredService<ILogger<CustomWebApplicationFactory>>();
+
+    db.Database.EnsureCreated();
+
+    try
     {
-      var host = builder.Build();
-      host.Start();
-
-      var serviceProvider = host.Services;
-
-      using var scope = serviceProvider.CreateScope();
-      var scopedServices = scope.ServiceProvider;
-      var db = scopedServices.GetRequiredService<AppDbContext>();
-
-      var logger = scopedServices
-        .GetRequiredService<ILogger<CustomWebApplicationFactory>>();
-
-      db.Database.EnsureCreated();
-
-      try
-      {
-        SeedData.PopulateTestData(db);
-      }
-      catch (Exception ex)
-      {
-        logger.LogError(ex, "An error occurred seeding the " +
-                            "database with test messages. Error: {exceptionMessage}", ex.Message);
-      }
-
-      return host;
+      SeedData.PopulateTestData(db);
+    }
+    catch (Exception ex)
+    {
+      logger.LogError(ex, "An error occurred seeding the " +
+                          "database with test messages. Error: {exceptionMessage}", ex.Message);
     }
 
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-      builder
-          .ConfigureServices(services =>
-          {
-            var descriptor = services.SingleOrDefault(
-              d => d.ServiceType ==
-                  typeof(DbContextOptions<AppDbContext>));
-
-            if (descriptor != null)
-            {
-              services.Remove(descriptor);
-            }
-
-            var inMemoryCollectionName = Guid.NewGuid().ToString();
-
-            services.AddDbContext<AppDbContext>(options =>
-              {
-                options.UseInMemoryDatabase(inMemoryCollectionName);
-              });
-          });
-    }
+    return host;
   }
 
+  protected override void ConfigureWebHost(IWebHostBuilder builder)
+  {
+    builder
+        .ConfigureServices(services =>
+        {
+          var descriptor = services.SingleOrDefault(
+            d => d.ServiceType ==
+                typeof(DbContextOptions<AppDbContext>));
+
+          if (descriptor != null)
+          {
+            services.Remove(descriptor);
+          }
+
+          var inMemoryCollectionName = Guid.NewGuid().ToString();
+
+          services.AddDbContext<AppDbContext>(options =>
+            {
+              options.UseInMemoryDatabase(inMemoryCollectionName);
+            });
+        });
+  }
 }
+
