5ab54d26de252ee990d6bfe3751e93d790cefa9f
Steve Smith <steve@kentsmiths.com>
2023-04-05 12:30:49 -0400

    Add new extension to produce StringContent

---
+++ src/Ardalis.HttpClientTestExtensions/HttpContentExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Ardalis.HttpClientTestExtensions;
+
+public static class HttpContentExtensionMethods
+{
+  public static StringContent FromModelAsJson(this HttpContent content, object model)
+  {
+    return new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json"));
+  }
+}
